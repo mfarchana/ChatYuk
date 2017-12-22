7874ba977367d03b875b5f7591ab7c15f0d9ff54
mfarchana <farchan@email.com>
2017-12-22 22:04:26 +0700

    Revisi Total 1.0

--- app/src/main/java/com/pencetcodestudio/chatyuk/MainActivity.java
+++ app/src/main/java/com/pencetcodestudio/chatyuk/MainActivity.java
@@ -1,116 +1,115 @@
 package com.pencetcodestudio.chatyuk;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.firebase.client.Firebase;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    private  Button add_room;
-    private  EditText room_name;
-    private ListView listView;
-    private String name;
-    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
+    int SIGN_IN_REQUEST_CODE = 1;
 
-    private ArrayAdapter<String> arrayAdapter;
-    private ArrayList<String> list_of_rooms = new ArrayList();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        add_room = (Button)findViewById(R.id.btnAdd_room);
-        room_name = (EditText)findViewById(R.id.etNeme_room);
-        listView = (ListView)findViewById(R.id.listView);
+        //Posisi login, apakah udah login atau belum
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_REQUEST_CODE);
+        } else {
+            Toast.makeText(this, "Hello " + FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), Toast.LENGTH_LONG).show();
+            displayMessages();
+        }
 
-        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list_of_rooms);
-        listView.setAdapter(arrayAdapter);
-
-        request_user_name();
-        add_room.setOnClickListener(new View.OnClickListener() {
+        //Kirim Pesan
+        FloatingActionButton fly = (FloatingActionButton) findViewById(R.id.fly_button);
+        fly.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Map<String,Object> map = new HashMap<String,Object>();
-                map.put(room_name.getText().toString(),"");
-                root.updateChildren(map);
-
+                //Deklarasi ID input
+                EditText input = (EditText) findViewById(R.id.input);
+                //Kirim data ke Database firebase
+                FirebaseDatabase.getInstance().getReference().push().setValue(new activity_chatroom(input.getText().toString(),
+                        FirebaseAuth.getInstance().getCurrentUser().getDisplayName()));
+                //Ambil data dari database firebase
+                input.setText("");
             }
         });
-        root.addValueEventListener(new ValueEventListener() {
+
+    }
+
+    //Tampilkan pesan
+    public void displayMessages() {
+        ListView listOfMessages = (ListView) findViewById(R.id.list_of_messages);
+        FirebaseListAdapter<activity_chatroom> adapter = new FirebaseListAdapter<activity_chatroom>
+                (this, activity_chatroom.class, R.layout.activity_chatroom, FirebaseDatabase.getInstance().getReference()) {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Set<String> set = new HashSet<String>();
-                Iterator i = dataSnapshot.getChildren().iterator();
-                while ( i.hasNext())
-                {
-                    set.add(((DataSnapshot)i.next()).getKey());
+            //model nama var
+            protected void populateView(View v, activity_chatroom model, int position) {
+                //Deklarasi id
+                TextView messageText = (TextView) v.findViewById(R.id.message_text);
+                TextView messageUser = (TextView) v.findViewById(R.id.message_user);
+                TextView messageTime = (TextView) v.findViewById(R.id.message_time);
+                //SetText (Ngambil data dari model)
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)", model.getMessageTime()));
+            }
+        };
+        listOfMessages.setAdapter(adapter);
+    }
+
+    //Posisi Login
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //OK
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Success!", Toast.LENGTH_LONG).show();
+                displayMessages();
+            }
+            //Bad
+            else {
+                Toast.makeText(this,
+                        "Please try again later.", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+    //Menu Logout
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_sign_out) {
+            AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Toast.makeText(MainActivity.this, "signed out.", Toast.LENGTH_LONG).show();
+                    finish();
                 }
-                list_of_rooms.clear();
-                list_of_rooms.addAll(set);
-
-                arrayAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                Intent I = new Intent(getApplicationContext(),activity_chatroom.class);
-                I.putExtra("room_name",((TextView)view).getText().toString());
-                I.putExtra("user_name",name);
-                startActivity(I);
-            }
-        });
-
+            });
+        }
+        return true;
     }
-
-    private void request_user_name() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Enter Name");
-        final EditText input_field = new EditText(this);
-        builder.setView(input_field);
-        builder.setPositiveButton("OK ", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                name = input_field.getText().toString();
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.cancel();
-                request_user_name();
-            }
-        });
-        builder.show();
-    }
-
 }
